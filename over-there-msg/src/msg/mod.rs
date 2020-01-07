21669abd799806205622be102df0e9d2c8b28982
Chip Senkbeil <chip@senkbeil.org>
2020-01-07 09:43:34 -0600

    Added tests to msg impl

--- over-there-msg/src/msg/mod.rs
+++ over-there-msg/src/msg/mod.rs
@@ -41,3 +41,109 @@ impl Msg {
         rmp_serde::from_read_ref(slice)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Serialize, Deserialize, Debug)]
+    enum Content1 {
+        Value1(String),
+        Value2(u32),
+    }
+
+    #[typetag::serde]
+    impl types::Content for Content1 {
+        fn as_any(&self) -> &dyn std::any::Any {
+            self
+        }
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    enum Content2 {
+        Value3(bool),
+    }
+
+    #[typetag::serde]
+    impl types::Content for Content2 {
+        fn as_any(&self) -> &dyn std::any::Any {
+            self
+        }
+    }
+
+    #[test]
+    fn is_content_should_yield_false_if_incorrect_concrete_type() {
+        let content = Content1::Value1(String::from("test"));
+        let msg = Msg::from_content(content);
+
+        assert!(!msg.is_content::<Content2>());
+    }
+
+    #[test]
+    fn is_content_should_yield_true_if_correct_concrete_type() {
+        let content = Content1::Value1(String::from("test"));
+        let msg = Msg::from_content(content);
+
+        assert!(msg.is_content::<Content1>());
+    }
+
+    #[test]
+    fn to_content_should_yield_none_if_incorrect_concrete_type() {
+        let content = Content1::Value1(String::from("test"));
+        let msg = Msg::from_content(content);
+
+        assert!(msg.to_content::<Content2>().is_none());
+    }
+
+    #[test]
+    fn to_content_should_yield_some_content_if_correct_concrete_type() {
+        let content = Content1::Value1(String::from("test"));
+        let msg = Msg::from_content(content);
+
+        let cast_content = msg
+            .to_content::<Content1>()
+            .expect("Failed to cast content");
+        match cast_content {
+            Content1::Value1(x) => assert_eq!(x, "test", "Content value was wrong"),
+            x => panic!("Unexpected content: {:?}", x),
+        }
+    }
+
+    #[test]
+    fn from_content_should_produce_a_new_msg_with_that_content() {
+        // Try a content type
+        let content = Content1::Value1(String::from("test"));
+        let msg = Msg::from_content(content);
+        assert!(
+            Utc::now()
+                .signed_duration_since(msg.creation_date)
+                .num_milliseconds()
+                >= 0,
+            "Unexpected creation date: {:?}",
+            msg.creation_date
+        );
+        match msg
+            .to_content::<Content1>()
+            .expect("Unable to cast content")
+        {
+            Content1::Value1(x) => assert_eq!(x, "test", "Content value was incorrect"),
+            x => panic!("Unexpected content: {:?}", x),
+        }
+
+        // Now try different content type
+        let content = Content2::Value3(true);
+        let msg = Msg::from_content(content);
+        assert!(
+            Utc::now()
+                .signed_duration_since(msg.creation_date)
+                .num_milliseconds()
+                >= 0,
+            "Unexpected creation date: {:?}",
+            msg.creation_date
+        );
+        let Content2::Value3(x) = msg
+            .to_content::<Content2>()
+            .expect("Unable to cast content");
+        assert!(x, "Content value was incorrect");
+    }
+}
