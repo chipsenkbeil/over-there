b5e178c5f9e304941ae04e7dc0025a4f85972e0a
Chip Senkbeil <chip@senkbeil.org>
2020-01-31 00:40:21 -0600

    Still failing -- need to see what part of read is causing failure

--- over-there-core/tests/scenarios/file.rs
+++ over-there-core/tests/scenarios/file.rs
@@ -6,18 +6,23 @@ pub async fn test(mut client: Client) {
     client.timeout = Duration::from_millis(2500);
 
     // Produce a new directory to work in
-    let dir = tempfile::TempDir::new().unwrap();
-    let dir_path = dir.path().to_string_lossy().to_string();
+    // let dir = tempfile::TempDir::new().unwrap();
+    // let dir_path = dir.path().to_string_lossy().to_string();
+    let dir = std::env::temp_dir();
+    let dir_path = dir.to_string_lossy().to_string();
 
     // Make a new file that we'll edit, read, and validate
-    let file_path = tempfile::NamedTempFile::new_in(&dir)
-        .unwrap()
-        .into_temp_path()
-        .to_string_lossy()
-        .to_string();
+    // let file_path = tempfile::NamedTempFile::new_in(&dir)
+    //     .unwrap()
+    //     .into_temp_path()
+    //     .to_string_lossy()
+    //     .to_string();
+    let mut file_path = dir.clone();
+    file_path.push("test_file.txt");
+    let file_path = file_path.to_string_lossy().to_string();
 
-    let dir_contents = client.ask_list_dir_contents(&dir_path).await.unwrap();
-    assert_eq!(dir_contents.len(), 0);
+    // let dir_contents = client.ask_list_dir_contents(&dir_path).await.unwrap();
+    // assert_eq!(dir_contents.len(), 0);
 
     let mut file = client.ask_open_file(&file_path).await.unwrap();
     client
@@ -25,8 +30,8 @@ pub async fn test(mut client: Client) {
         .await
         .unwrap();
 
-    let dir_contents = client.ask_list_dir_contents(&dir_path).await.unwrap();
-    assert_eq!(dir_contents.len(), 1);
+    // let dir_contents = client.ask_list_dir_contents(&dir_path).await.unwrap();
+    // assert_eq!(dir_contents.len(), 1);
 
     let result =
         String::from(std::str::from_utf8(&client.ask_read_file(&file).await.unwrap()).unwrap());
