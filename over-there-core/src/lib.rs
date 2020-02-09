0189d2d2190b4e24b97ece5a1976cd8f954d5a96
Chip Senkbeil <chip@senkbeil.org>
2020-02-09 17:33:36 -0600

    Added ExecAskError as export

--- over-there-core/src/lib.rs
+++ over-there-core/src/lib.rs
@@ -2,7 +2,7 @@ mod client;
 mod msg;
 mod server;
 
-pub use client::{AskError, Client, FileAskError, TellError};
+pub use client::{AskError, Client, ExecAskError, FileAskError, TellError};
 pub use msg::{
     content::{self, Content},
     Msg, MsgError,
