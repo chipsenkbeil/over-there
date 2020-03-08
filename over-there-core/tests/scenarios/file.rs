189614e6dc5b300040ffcb7348969a465ecf6b0e
Chip Senkbeil <chip@senkbeil.org>
2020-03-07 21:55:19 -0600

    Updated file manipulation test to use full suite of file operations

--- over-there-core/tests/scenarios/file.rs
+++ over-there-core/tests/scenarios/file.rs
@@ -15,22 +15,62 @@ pub async fn async_test(mut client: Client) {
     let dir_contents = client
         .ask_list_dir_contents(dir_path.clone())
         .await
-        .unwrap();
+        .expect("Failed to get empty dir contents");
     assert_eq!(dir_contents.len(), 0);
 
     // Open/create file with read & write access
-    let mut file = client.ask_open_file(file_path).await.unwrap();
+    let mut file = client
+        .ask_open_file(file_path.clone())
+        .await
+        .expect("Failed to open file");
     client
         .ask_write_file(&mut file, b"Hello!\nThis is a test!\nGoodbye!")
         .await
-        .unwrap();
+        .expect("Failed to write to file");
 
-    let dir_contents = client.ask_list_dir_contents(dir_path).await.unwrap();
+    let dir_contents = client
+        .ask_list_dir_contents(dir_path.clone())
+        .await
+        .expect("Failed to get dir contents");
     assert_eq!(dir_contents.len(), 1);
+
+    // Close the file, rename it, re-open it, read content
+    client
+        .ask_close_file(&file)
+        .await
+        .expect("Failed to close file");
+
+    let file_path_2 = format!("{}.2", file_path.clone());
+    client
+        .ask_rename_file(file_path, file_path_2.clone())
+        .await
+        .expect("Failed to rename file");
+
+    let file = client
+        .ask_open_file(file_path_2.clone())
+        .await
+        .expect("Failed to open renamed file");
 
     let result = String::from(
         std::str::from_utf8(&client.ask_read_file(&file).await.unwrap())
-            .unwrap(),
+            .expect("Failed to read renamed file"),
     );
     assert_eq!(result, "Hello!\nThis is a test!\nGoodbye!");
+
+    // Close file and remove it
+    client
+        .ask_close_file(&file)
+        .await
+        .expect("Failed to close file");
+    client
+        .ask_remove_file(file_path_2)
+        .await
+        .expect("Failed to remove renamed file");
+
+    // Verify it's gone
+    let dir_contents = client
+        .ask_list_dir_contents(dir_path)
+        .await
+        .expect("Failed to get dir contents");
+    assert_eq!(dir_contents.len(), 0);
 }
